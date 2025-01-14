e835eb19aac38f83bb9e86c84edd564f8b35060e
massimiliano <massimarcello@hotmail.it>
2025-01-14 13:33:07 +0100

    contact done

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -8,44 +8,62 @@ const Contact = () => {
       <div className={styles.contactContainer} id="contact">
         <h1>Contact</h1>
         <div className={styles.contactContent}>
+        <div className={styles.getInTouchContainer} >
           <h2 className={styles.titleContact}>Get in touch</h2>
           <p className={styles.titleDescription}>
             If you'd like to discuss a project, collaboration, or speaking
             opportunity, feel free to reach out.
           </p>
           <div>
-            <Link href="mailto:massdev@gmail.com">
-              <p className={styles.mailLink}>Email: massdev@gmail.com</p>
+            <Link href="mailto:massdev@gmail.com" className={styles.link}>
+              <span className={styles.label}> Email: </span>    <span className={styles.labelContent}>massdev@gmail.com</span> 
             </Link>
-            <a
+            <Link
               href="https://www.linkedin.com/in/massimiliano-marcello-4195681a7/"
               target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
             >
-              <p  className={styles.Linkdin}>
-                LinkedIn:
-                https://www.linkedin.com/in/massimiliano-marcello-4195681a7/
-              </p>
+              <span className={styles.label}> LinkedIn:</span>{" "}
+           <span className={styles.labelContent}>   https://www.linkedin.com/in/massimiliano-marcello-4195681a7/</span>
+            </Link>
+            <a
+              href="mailto:massdev@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
             </a>
-            <a href="mailto:massdev@gmail.com" target="_blank">
-            
-              <button className={styles.messageButton}>
-                Send a Message</button>
-            </a>
+            <button className={styles.messageButton}>Send a Message</button>
+            </div>
           </div>
-          <div>
+          <div className={styles.followMeContainer} >
             <h2 className={styles.titleContact}>Follow Me</h2>
             <p className={styles.titleDescription}>
               Stay updated with my latest projects, thoughts, and insights.
             </p>
-            <a href="https://x.com/MassDev" target="_blank">
-              <p className={styles.twitter}>Twitter: @massdev</p>
-            </a>
-            <a href="https://github.com/massimilianomarcello" target="_blank">
-              <p className={styles.github}>GitHub: github.com/massimilianomarcello</p>
-            </a>
-            <a href="https://x.com/MassDev" target="_blank">
-              <button className={styles.followButton}>Follow on Twitter</button>
-            </a>
+            <Link
+              href="https://x.com/MassDev"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              <span className={styles.label}> Twitter:</span>    <span className={styles.labelContent}> @massdev</span>
+            </Link>
+            <Link
+              href="https://github.com/massimilianomarcello"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              <span className={styles.label}> GitHub:</span>    <span className={styles.labelContent}> github.com/massimilianomarcello</span>
+            </Link>
+            <a
+              href="https://x.com/MassDev"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+            <button className={styles.followButton}>Follow on Twitter</button>
           </div>
         </div>
       </div>
